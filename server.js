4f993842d0d37596ea253d31d06f69325345cced
lingtran <lxiutran@gmail.com>
2016-07-13 09:54:52 -0600

    client sends messages to server

--- server.js
+++ server.js
@@ -20,16 +20,30 @@ const socketIo = require('socket.io');
 const io = socketIo(server);
 
 io.on('connection', function(socket){
+  var votes = {};
   console.log('A user has connected.', io.engine.clientsCount);
 
   io.sockets.emit('usersConnected', io.engine.clientsCount);
 
   socket.emit('statusMessage', 'You have connected.');
 
+  socket.on('message', function(channel, message){
+    console.log(channel, message);
+
+    if(channel === 'voteCast'){
+      votes[socket.id] = message;
+      console.log(votes);
+    }
+  });
+
   socket.on('disconnect', function(){
     console.log('A user has disconnected.', io.engine.clientsCount);
+    delete votes[socket.id];
+    console.log(votes);
     io.sockets.emit('userConnection', io.engine.clientsCount);
   });
+
 });
+
 
 module.exports = server;
